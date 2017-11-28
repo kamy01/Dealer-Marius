69a39bcc4bb25d6f4a79f99de7396daf739f127d
marius <mariusdotspinu@gmail.com>
2017-11-28 16:34:57 +0200

    Added methods for Car

--- business-logic/src/main/java/com/dealer/dao/interfaces/Validator.java
+++ business-logic/src/main/java/com/dealer/dao/interfaces/Validator.java
@@ -1,10 +1,16 @@
 package com.dealer.dao.interfaces;
 
+import com.dealer.dto.Car;
 import com.dealer.dto.Dealer;
+import com.dealer.entities.CarEntity;
 import com.dealer.entities.DealerEntity;
 
 public interface Validator {
     Dealer getDealerFromDealerEntity(DealerEntity dealerEntity);
     Dealer findDealer(Dealer dealer);
     boolean createDealer(String dealerName, String dealerPassword);
+
+    Car getCarFromCarEntity(CarEntity carEntity);
+    Car findCar(Car car);
+    boolean createCar(String name, String mark, String color, String price, String condition, String date);
 }
