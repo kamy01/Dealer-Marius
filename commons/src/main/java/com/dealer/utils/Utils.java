bbfc804bc6aa0ec41b644240a2522abcdbf435a5
marius <mariusdotspinu@gmail.com>
2017-11-28 15:37:27 +0200

    Added Car validation constants

--- commons/src/main/java/com/dealer/utils/Utils.java
+++ commons/src/main/java/com/dealer/utils/Utils.java
@@ -5,4 +5,11 @@ public class Utils {
     public static final int NOT_FOUND = 404;
     public static final int SUCCESS = 200;
     public static final int UNAUTHORIZED = 401;
+
+    public static final String DATE_PATTERN = "dd/MM/yyyy";
+    public static final String REGISTRATION_DATE_VALIDATOR_NAME = "registrationDateValidator";
+    public static final String REGISTRATION_DATE_AFTER_CURRENT_ERROR =
+            "Registration Date cannot be after current date !";
+    public static final String INVALID_DATE_FORMAT_ERROR = "Invalid Date Format !";
+
 }
