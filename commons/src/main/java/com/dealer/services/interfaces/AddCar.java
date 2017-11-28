f163802526284bf00318526d0b9a7c7ca6bc3359
marius <mariusdotspinu@gmail.com>
2017-11-28 18:03:25 +0200

    Changed String date to Date date

--- commons/src/main/java/com/dealer/services/interfaces/AddCar.java
+++ commons/src/main/java/com/dealer/services/interfaces/AddCar.java
@@ -1,7 +1,8 @@
 package com.dealer.services.interfaces;
 
+import javax.ejb.Remote;
 import java.util.Date;
-
+@Remote
 public interface AddCar {
     boolean isCarAdded(String name, String mark, String color, String price, String condition, Date date);
 }
