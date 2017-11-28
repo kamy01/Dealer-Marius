19c453382af6e75fe862c8fe457ababd187a290d
marius <mariusdotspinu@gmail.com>
2017-11-28 18:08:23 +0200

    DealerValidator kept only for dealers

--- business-logic/src/main/java/com/dealer/dao/interfaces/DealerValidator.java
+++ business-logic/src/main/java/com/dealer/dao/interfaces/DealerValidator.java
@@ -1,16 +1,11 @@
 package com.dealer.dao.interfaces;
 
-import com.dealer.dto.Car;
 import com.dealer.dto.Dealer;
-import com.dealer.entities.CarEntity;
 import com.dealer.entities.DealerEntity;
 
-public interface Validator {
+
+public interface DealerValidator {
     Dealer getDealerFromDealerEntity(DealerEntity dealerEntity);
     Dealer findDealer(Dealer dealer);
     boolean createDealer(String dealerName, String dealerPassword);
-
-    Car getCarFromCarEntity(CarEntity carEntity);
-    Car findCar(Car car);
-    boolean createCar(String name, String mark, String color, String price, String condition, String date);
 }
