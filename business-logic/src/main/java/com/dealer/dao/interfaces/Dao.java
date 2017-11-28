3e2083fae524fa88cab20760df4b7d4f29cbbcfa
marius <mariusdotspinu@gmail.com>
2017-11-28 16:33:47 +0200

    Added methods for CarEntity

--- business-logic/src/main/java/com/dealer/dao/interfaces/Dao.java
+++ business-logic/src/main/java/com/dealer/dao/interfaces/Dao.java
@@ -1,9 +1,14 @@
 package com.dealer.dao.interfaces;
 
+import com.dealer.entities.CarEntity;
 import com.dealer.entities.DealerEntity;
 
 public interface Dao {
     boolean createDealerEntity(String dealerName, String dealerPassword);
     void deleteDealerEntity(String dealerName);
     DealerEntity getDealerEntity(String dealerName);
+
+    boolean createCarEntity(String name, String mark, String color, String price, String condition, String date);
+    void deleteCarEntity(int id, int ownerId);
+    CarEntity getCarEntity(int id);
 }
