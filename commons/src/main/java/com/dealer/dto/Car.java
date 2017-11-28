7be601d14b1ea4aff498adfd21907311383bb661
marius <mariusdotspinu@gmail.com>
2017-11-28 16:35:26 +0200

    Added Car dto

---
+++ commons/src/main/java/com/dealer/dto/Car.java
@@ -0,0 +1,79 @@
+package com.dealer.dto;
+
+import java.io.Serializable;
+
+public class Car implements Serializable {
+    private String name;
+    private String mark;
+    private String condition;
+    private String registrationDate;
+    private String color;
+    private String price;
+
+    private int id;
+    private int ownerId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+}
