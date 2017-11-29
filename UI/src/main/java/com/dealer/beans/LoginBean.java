508b4942d82b509d9a30079309e2dfcc07d5823e
marius <mariusdotspinu@gmail.com>
2017-11-29 12:13:11 +0200

    Added login info messages

--- UI/src/main/java/com/dealer/beans/LoginBean.java
+++ UI/src/main/java/com/dealer/beans/LoginBean.java
@@ -1,10 +1,14 @@
 package com.dealer.beans;
 
 import com.dealer.services.interfaces.Login;
+import com.dealer.utils.Utils;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
 import java.io.Serializable;
 
 @ManagedBean
@@ -19,11 +23,28 @@ public class LoginBean implements Serializable {
 
     private String dealerName;
     private String dealerPassword;
+    private UIComponent loginBtn;
 
     public LoginBean(){}
 
     public void checkLogIn(){
         requestCode = login.getResponseCode(dealerName, dealerPassword);
+
+        FacesMessage unauthorizedMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR,Utils.LOGIN_UNAUTHORIZED_MESSAGE,
+                Utils.LOGIN_UNAUTHORIZED_MESSAGE);
+
+        FacesMessage notFoundMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR,Utils.LOGIN_NOT_FOUND_MESSAGE,
+                Utils.LOGIN_NOT_FOUND_MESSAGE);
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        switch (requestCode){
+            case Utils.NOT_FOUND:
+                context.addMessage(loginBtn.getClientId(context), notFoundMsg);
+                break;
+            case Utils.UNAUTHORIZED:
+                context.addMessage(loginBtn.getClientId(context), unauthorizedMsg);
+                break;
+        }
 
     }
 
@@ -45,4 +66,11 @@ public class LoginBean implements Serializable {
 
     public int getRequestCode(){return requestCode;}
 
+    public UIComponent getLoginBtn() {
+        return loginBtn;
+    }
+
+    public void setLoginBtn(UIComponent loginBtn) {
+        this.loginBtn = loginBtn;
+    }
 }
