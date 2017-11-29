19996c3f51f73cd48c13f16165a6b61062912b8d
marius <mariusdotspinu@gmail.com>
2017-11-29 12:13:55 +0200

    Added register info messages

--- UI/src/main/java/com/dealer/beans/RegisterBean.java
+++ UI/src/main/java/com/dealer/beans/RegisterBean.java
@@ -1,10 +1,14 @@
 package com.dealer.beans;
 
 import com.dealer.services.interfaces.Register;
+import com.dealer.utils.Utils;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
 import java.io.Serializable;
 
 @ManagedBean
@@ -15,6 +19,7 @@ public class RegisterBean implements Serializable {
     private String dealerName;
     private String dealerPassword;
     private boolean isCreated;
+    private UIComponent registerBtn;
 
     @EJB
     private Register register;
@@ -33,6 +38,20 @@ public class RegisterBean implements Serializable {
 
     public void createDealer(){
         isCreated = register.isCreated(dealerName, dealerPassword);
+
+        FacesMessage successMsg = new FacesMessage(FacesMessage.SEVERITY_INFO,Utils.REGISTER_SUCCESS_MESSAGE,
+                Utils.REGISTER_SUCCESS_MESSAGE);
+
+        FacesMessage failedMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR,Utils.REGISTER_USER_EXISTS_MESSAGE,
+                Utils.REGISTER_USER_EXISTS_MESSAGE);
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        if(isCreated){
+            context.addMessage(registerBtn.getClientId(context), successMsg);
+        }
+        else{
+            context.addMessage(registerBtn.getClientId(context), failedMsg);
+        }
     }
 
     public String getDealerName(){
@@ -44,4 +63,12 @@ public class RegisterBean implements Serializable {
     }
 
     public boolean getIsCreated(){return isCreated;}
+
+    public UIComponent getRegisterBtn() {
+        return registerBtn;
+    }
+
+    public void setRegisterBtn(UIComponent registerBtn) {
+        this.registerBtn = registerBtn;
+    }
 }
